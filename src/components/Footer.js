84e950a8b0c9ee4192e718c6bf3c68b2f6d0a712
tmaraki <toshiemaraki@gmail.com>
2024-04-25 21:32:33 -0400

    added about me page layout. Added social media icon images and footer context

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,8 @@
 //contains links to github,linkedin and other platforms
 
-export default function Footer() {}
+<footer>
+    <ul class="social-icons">
+        <li><a href="https://github.com/tmaraki" target="_blank"><img src="../public/github.png" alt="GitHub"></a></li>
+        <li><a href="https://www.linkedin.com/in/toshie-araki/" target="_blank"><img src="../public/linkedin.webp" alt="LinkedIn"></a></li>
+    </ul>
+</footer>
