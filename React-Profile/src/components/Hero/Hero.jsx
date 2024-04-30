a0a1b54aca54a21006610231e9e675cb32b2d31b
tmaraki <toshiemaraki@gmail.com>
2024-04-30 15:00:46 -0400

    Added new Hero component for the home page

---
+++ React-Profile/src/components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+    return (
+    <section>
+        <div>
+            <h1>Hi, I'm Toshie</h1>
+            <p>
+            I'm a new full-stack developer with previous experience in project management seeking a new adventure as a developer. Reach out if you'd like to learn more!
+            </p>
+            <a href="mailto:toshiemaraki@gmail.com">Contact Me</a>
+        </div>
+        <img src=[getImageUrl]
+    </section>
+    );
+};
