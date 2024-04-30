41fc73984c04d5c4fc7d3ac4acba62363842b070
tmaraki <toshiemaraki@gmail.com>
2024-04-30 12:01:47 -0400

    started over from scratch. installed Vite and started set up

--- src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-//contains links to github,linkedin and other platforms
-
-<footer>
-    <ul class="social-icons">
-        <li><a href="https://github.com/tmaraki" target="_blank"><img src="../public/github.png" alt="GitHub"></a></li>
-        <li><a href="https://www.linkedin.com/in/toshie-araki/" target="_blank"><img src="../public/linkedin.webp" alt="LinkedIn"></a></li>
-    </ul>
-</footer>
